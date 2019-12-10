3b4d8148b70682be0bf55120008322a2c5658c91
qamar mudhafar <qamarmudhafar@yahoo.com>
2019-12-10 23:56:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 // Your code here
-class Polygon {
-  constructor(sides) {
-    this.sides = sides
-    this.count = this.sides.length
+class Tree {
+  constructor( species ) {
+    this.species = species
   }
 
-  get countSides() {
-    return this.sides.length
+  static definition() {
+    return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
   }
 
-  get perimeter() {
-    if (!Array.isArray(this.sides)) return;
-    let sum = 0;
-    for (var int of this.sides) {
-      sum += int
-    }
-    return sum
+}
+
+class Deciduous extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
   }
 }
 
-class Triangle extends Polygon {
-
-  get isValid() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 3) return;
-    let side1 = this.sides[0]
-    let side2 = this.sides[1]
-    let side3 = this.sides[2]
-    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
-  }
-}
-
-class Square extends Polygon {
-
-  get isValid() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 4) return;
-    let side1 = this.sides[0]
-    let side2 = this.sides[1]
-    let side3 = this.sides[2]
-    let side4 = this.sides[3]
-    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+class Evergreen extends Tree {
+  constructor( species, name ) {
+    super( species )
+    this.name = name
   }
 
-  get area() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 4) return;
-    return this.sides[0] * this.sides[0]
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`
   }
 }
